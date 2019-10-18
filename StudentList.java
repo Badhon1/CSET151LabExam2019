c26a470305a4b41c20af2aaf7b03399eb6513770
Badhon <iamtanvir.cse@gmail.com>
2019-10-18 10:27:10 -0600

    STEP #5.Replaces string literals with constants, storing those constants in a new class called Constants.java

--- StudentList.java
+++ StudentList.java
@@ -1,69 +1,59 @@
 import java.io.*;
 import java.text.*;
 import java.util.*;
+import static  ase.Constants.*;
 public class StudentList 
 {   
 		public  static String LoadData()
 		{
-		System.out.println("Loading data ...");
+		System.out.println("StartDialog");
 		String contents = null;
-		try {
+		try 
+		{
 			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt")));
+							new FileInputStream("DataFile")));
 			 contents = fileStream.readLine();
-		} catch (Exception e){}
-		return  contents;
+		} catch (Exception e)
+		{
+
+		}
 	}
 
 	public  static  void  WirteData(String[] args){
-		System.out.println("Loading data ...");
+		System.out.println("StartDialog");
 		try {
 			BufferedWriter s = new BufferedWriter(
-					new FileWriter("students.txt", true));
-			String t = args[0].substring(1);
-			Date d = new Date();
-			String df = "dd/mm/yyyy-hh:mm:ss a";
-			DateFormat dateFormat = new SimpleDateFormat(df);
-			String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
+					new FileWriter("DataFile", true));
 			s.close();
 		} catch (Exception e){}
 
-		System.out.println("Data Loaded.");
+		System.out.println("EndDialog.");
 	}
 
 	public static void main(String[] args) 
 	{
         if(args.length == 0){
-		    System.out.println("Usage: java ase.StudentList (a|r|c|?word|+word)");
+		    System.out.println(Usage);
 		    return;
 	}
 //		Check arguments
 		if(args[0].equals("a")) 
 		{
             String contents = LoadData();
-
-
-
-
-
-
-			String words[] = contents.split(",");			
+            String words[] = contents.split(",");			
 			for(String word : words) 
 				{ 
 					System.out.println(word); 
 				} 
-			System.out.println("Data Loaded.");
+			System.out.println("EndDialog");
 		}
 		else if(args[0].equals("r")) 
-		{
-            String contents = LoadData();
-			System.out.println(contents);
-			String words[] = contents.split(",");	
+		{	
 			Random x = new Random();
 				int y = x.nextInt();
 					System.out.println(words[y]);
+					System.out.println(EndDialog);
 			} 			
 		else if(args[0].contains("+"))
 		{
@@ -71,19 +61,16 @@ public class StudentList
 		}
 		else if(args[0].contains("?")) 
 		{
-            String contents = LoadData();
-			String words[] = contents.split(",");	
-			boolean done = false;
 			String t = args[0].substring(1);
 			for(int idx = 0; idx<words.length && !done; idx++) 
 			{
 				if(words[idx].equals(t)) 
 				{
-					System.out.println("We found it!");
+					System.out.println("Success");
 						done=true;
 				}
 			} 
-              System.out.println("Data Loaded.");			
+              System.out.println("EndDialog");			
 		}
 		else if(args[0].contains("c")) 
 		{
@@ -103,7 +90,7 @@ public class StudentList
 				}
 			}
 			System.out.println(count +" word(s) found " + a.length);
-            System.out.println("Data Loaded.");			
+            System.out.println("EndDialog");			
 		}
 
 	}	
